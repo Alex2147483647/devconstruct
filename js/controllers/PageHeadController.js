6ede8ef75181373d572ea558d3d42d36890ffd5f
Alex <alex2147483647@gmail.com>
2015-09-13 09:49:32 +0300

    States fix

--- js/controllers/PageHeadController.js
+++ js/controllers/PageHeadController.js
@@ -1,5 +1,6 @@
 devConstruct.controller('PageHeadController', function ($rootScope, $scope, $http, $timeout, Categories) {
     $scope.$on('$viewContentLoaded', function () {
+        console.log('AppController');
         $rootScope.listCategories = Categories.find({
             where: {
                 type: "freelance"

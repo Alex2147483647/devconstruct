b7200fee83987b3aef8a5506ad3f8fca853a1ed2
Alex <alex2147483647@gmail.com>
2015-09-14 15:55:04 +0300

    States fix

--- js/app.js
+++ js/app.js
@@ -1,8 +1,27 @@
-angular.module('devConstruct', [
-    'ui.router',
-    "lbServices"
-]);
+(function (window, angular, undefined) {
+    /*jshint globalstrict:true*/
+    /*global angular:false*/
+    'use strict';
 
-devConstruct.controller('AppController', function ($scope) {
+    angular.module('app', [
+        'ui.router',
+        "lbServices",
+        'services.breadcrumbs'
+    ]);
 
-});
+    angular.module('app').config(['$routeProvider', '$locationProvider', function ($routeProvider, $locationProvider) {
+        $locationProvider.html5Mode(true);
+        $routeProvider.otherwise({redirectTo: '/404'});
+    }]);
+
+    angular.module('app').controller('AppController', function ($scope) {
+
+    });
+
+    angular.module('app').controller('HeaderController', ['$scope', '$location', '$route', 'lbServices', 'breadcrumbs',
+        function ($scope, $location, $route, breadcrumbs) {
+            $scope.location = $location;
+            $scope.breadcrumbs = breadcrumbs;
+        }
+    ]);
+})(window, window.angular);

d54dbf90805531f53ea7f46f9146508d83a7b63e
Alex <alex2147483647@gmail.com>
2015-09-14 12:02:16 +0300

    States fix

--- js/app.js
+++ js/app.js
@@ -392,6 +392,13 @@ devConstruct.controller('PageHeadController', ['$scope', function ($scope, Categ
 }]);
 
 
+devConstruct.controller('AppController', ['$scope', '$rootScope', function ($scope, $rootScope) {
+    $scope.$on('$viewContentLoaded', function () {
+        Metronic.initComponents(); // init core components
+        Layout.init(); //  Init entire layout(header, footer, sidebar, etc) on page load if the partials included in server side instead of loading with ng-include directive
+    });
+}]);
+
 /* Init global settings and run the app */
 devConstruct.run(["$rootScope", "settings", "$state", function ($rootScope, settings, $state) {
     $rootScope.$state = $state;
